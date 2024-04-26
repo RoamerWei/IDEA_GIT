f529b0dfdfe06bd2a9170029cf998cff02e762d5
BardaMu <3150wyz@gmail.com>
2024-04-26 14:44:31 +0800

    第五次使用idea提交代码

--- src/com/kono/git/HelloWorld.java
+++ src/com/kono/git/HelloWorld.java
@@ -3,7 +3,5 @@ package com.kono.git;
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
-        System.out.println("HelloWorld");
     }
 }
